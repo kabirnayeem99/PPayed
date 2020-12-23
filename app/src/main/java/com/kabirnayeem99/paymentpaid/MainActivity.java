ffdf166d54c07a59bca4af9758bd0331c73601ed
Naimul Kabir <kabirnayeem.99@gmail.com>
2020-12-23 22:21:57 +0600

    📦 new: creates two new activities
    creates activity main and activity work

---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/MainActivity.java
@@ -0,0 +1,61 @@
+package com.kabirnayeem99.paymentpaid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button worksButton;
+    Button paymentButton;
+    Button exportButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+        clickWorksButton();
+        clickPaymentButton();
+        clickExportButton();
+    }
+
+    private void clickExportButton() {
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Navigates to Export page", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void clickPaymentButton() {
+        paymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Navigates to Payment page", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void clickWorksButton() {
+        worksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Navigates to Works page", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void initViews() {
+        worksButton = findViewById(R.id.works_button);
+        paymentButton = findViewById(R.id.payments_button);
+        exportButton = findViewById(R.id.exports_button);
+    }
+}
---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/WorksActivity.java
@@ -0,0 +1,14 @@
+package com.kabirnayeem99.paymentpaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_works);
+    }
+}
