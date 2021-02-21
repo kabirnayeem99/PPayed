f092497478f04d0aee656538e567eec603242edb
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-02-21 15:19:52 +0600

    📦 new: adds sign in functionality

---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/auth/AuthService.kt
@@ -0,0 +1,35 @@
+package com.kabirnayeem99.paymentpaid.auth
+
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.kabirnayeem99.paymentpaid.ui.activities.TAG
+import com.kabirnayeem99.paymentpaid.utils.Resource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class AuthService {
+    suspend fun registerUser(email: String, password: String, auth: FirebaseAuth): Resource<FirebaseAuth> {
+        return try {
+            auth.createUserWithEmailAndPassword(email, password).await()
+            Resource.Success(auth)
+        } catch (e: Exception) {
+            Log.d(TAG, "onCreate: ${e.message}")
+            Resource.Error(e.message ?: "Error", auth)
+        }
+
+    }
+
+    suspend fun loginUser(email: String, password: String, auth: FirebaseAuth) {
+        try {
+            auth.signInWithEmailAndPassword(email, password).await()
+        } catch (e: Exception) {
+            Log.d(TAG, "onCreate: ${e.message}")
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/ui/activities/SignInActivity.kt
@@ -0,0 +1,115 @@
+package com.kabirnayeem99.paymentpaid.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.kabirnayeem99.paymentpaid.R
+import com.kabirnayeem99.paymentpaid.auth.AuthService
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+const val TAG = "SignInActivity"
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var authService: AuthService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        handleAuthentication()
+        authService = AuthService()
+
+        auth.signOut()
+
+        if (auth.currentUser != null) {
+            moveToMainActivity()
+        }
+
+    }
+
+    private fun moveToMainActivity() {
+        if (auth.currentUser != null) {
+            Toast.makeText(this,
+                    "You are signed in with ${auth.currentUser!!.email}",
+                    Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this,
+                    "You signed in without any online syncing support.",
+                    Toast.LENGTH_LONG).show()
+        }
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        this.finish()
+    }
+
+    private fun handleAuthentication() {
+        auth = FirebaseAuth.getInstance()
+        handleLogin()
+        handleRegistration()
+        handleWithoutLogin()
+    }
+
+    private fun handleWithoutLogin() {
+        Log.d(TAG, "handleWithoutLogin: without logging ing")
+        btnWithoutLogin.setOnClickListener {
+            pbSigningIn.visibility = View.VISIBLE
+            moveToMainActivity()
+        }
+    }
+
+    private fun handleRegistration() {
+        btnRegister.setOnClickListener {
+            val email: String = etEmailAddress.text.toString()
+            val password: String = etPassword.text.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    withContext(Dispatchers.Main) {
+                        pbSigningIn.visibility = View.VISIBLE
+                    }
+                    authService.registerUser(email, password, auth)
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@SignInActivity, "${e.message}",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                    withContext(Dispatchers.Main) {
+                        pbSigningIn.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleLogin() {
+        btnLogIn.setOnClickListener {
+            val email: String = etEmailAddress.text.toString()
+            val password: String = etPassword.text.toString()
+            try {
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    withContext(Dispatchers.Main) {
+                        pbSigningIn.visibility = View.VISIBLE
+                    }
+                    authService.loginUser(email, password, auth)
+                    withContext(Dispatchers.Main) {
+                        moveToMainActivity()
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, "onCreate: ${e.message}")
+                Toast.makeText(this@SignInActivity,
+                        "${e.message}", Toast.LENGTH_SHORT).show()
+                pbSigningIn.visibility = View.GONE
+            }
+        }
+    }
+}
