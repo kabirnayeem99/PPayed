b6c796a7545e67f8486afb591e09ba6cfaad644a
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-02-27 16:14:00 +0600

    📦 new: adds analytics fragment

---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/ui/fragments/AnalyticsFragment.kt
@@ -0,0 +1,109 @@
+package com.kabirnayeem99.paymentpaid.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat.getColor
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.kabirnayeem99.paymentpaid.R
+import com.kabirnayeem99.paymentpaid.enums.Months
+import com.kabirnayeem99.paymentpaid.ui.WorkViewModel
+import com.kabirnayeem99.paymentpaid.ui.activities.HomeActivity
+import com.kabirnayeem99.paymentpaid.utils.CustomUtils
+import kotlinx.android.synthetic.main.fragment_analytics.*
+import kotlin.collections.ArrayList
+
+
+class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
+
+    private lateinit var workViewModel: WorkViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViewModel()
+        initGraph()
+
+    }
+
+    /**
+     * Sets up the [WorkViewModel] for this [WorkFragment]
+     */
+    private fun setUpViewModel() {
+        workViewModel = (activity as HomeActivity).workViewModel
+    }
+
+    private fun initGraph() {
+
+        workViewModel.getTotalPaymentsByMonth().observe(viewLifecycleOwner, { paymentList ->
+            when (paymentList == null) {
+                true -> showLoading()
+                false -> {
+                    hideLoading()
+                    implementDataSeries(paymentList)
+                }
+            }
+        })
+    }
+
+    private fun implementDataSeries(paymentList: List<Int>) {
+
+        val months = Months.values()
+
+        val barEntries = ArrayList<BarEntry>()
+        val pieEntries = ArrayList<PieEntry>()
+
+        var position = 0f
+        paymentList.forEach {
+            barEntries.add(BarEntry(position, it.toFloat()))
+            if (it > 0) {
+                pieEntries.add(PieEntry(it.toFloat(), CustomUtils.getCurrentMonthName(position.toInt())))
+            }
+            position += 1f
+        }
+
+        val barDataSet = BarDataSet(barEntries, "Payments")
+        val pieDataSet = PieDataSet(pieEntries, "payments")
+
+        val barData = BarData(barDataSet)
+        val pieData = PieData(pieDataSet)
+        barChart.data = barData
+        pieChart.data = pieData
+
+        barDataSet.valueTextColor = getColor(requireContext(), R.color.material_white)
+        barDataSet.color = getColor(requireContext(), R.color.material_dark_green_dark)
+        barDataSet.colors = CustomUtils.getColorsFromTemplate()
+        pieDataSet.valueTextColor = getColor(requireContext(), R.color.material_white)
+        pieDataSet.color = getColor(requireContext(), R.color.material_dark_green_dark)
+        pieDataSet.colors = CustomUtils.getColorsFromTemplate()
+
+
+        barChart.animateY(1000)
+        pieChart.animateY(1000)
+
+    }
+
+
+    private fun showLoading() {
+        progressBarAnalytics.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        progressBarAnalytics.visibility = View.INVISIBLE
+    }
+
+    private fun getColors(): ArrayList<Int> {
+        val colors = ArrayList<Int>()
+        colors.add(CustomUtils.getRandomColor())
+        colors.add(CustomUtils.getRandomColor())
+        colors.add(CustomUtils.getRandomColor())
+        colors.add(CustomUtils.getRandomColor())
+        colors.add(CustomUtils.getRandomColor())
+
+        return colors
+    }
+
+    companion object {
+        private const val TAG = "AnalyticsFragment"
+    }
+}
