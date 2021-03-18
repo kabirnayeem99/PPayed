c33e9bcb469896e2cdf37e49922724e75e80ce92
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-18 17:03:59 +0600

    📦 new: adds splash screen
    * implements motion layout
    * adds new splash activity
    * implements timer

---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/ui/activities/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.kabirnayeem99.paymentpaid.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kabirnayeem99.paymentpaid.R
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
+
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var timer: Timer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(applicationContext, SignInActivity::class.java))
+            }
+        }, 900)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
+    }
+}
