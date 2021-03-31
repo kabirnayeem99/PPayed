abc787bbb6cb1ee6d29ffe811612a497e4349f4a
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-31 23:43:14 +0600

    📦 feat (profile settings): finish settings page design

--- app/src/main/java/com/kabirnayeem99/paymentpaid/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/ui/fragments/ProfileFragment.kt
@@ -1,23 +1,88 @@
 package com.kabirnayeem99.paymentpaid.ui.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.kabirnayeem99.paymentpaid.R
+import com.kabirnayeem99.paymentpaid.adapters.ProfileSettingsAdapter
+import com.kabirnayeem99.paymentpaid.adapters.ProfileSettingsItem
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.coroutines.flow.Flow
 import java.util.*
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val auth = FirebaseAuth.getInstance()
+    private val user: FirebaseUser? = auth.currentUser
+
+    val settingsItemListLiveData: MutableLiveData<List<ProfileSettingsItem>> = MutableLiveData()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setData()
         super.onViewCreated(view, savedInstanceState)
+        setData()
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        val profileAdapter = ProfileSettingsAdapter()
+        rvSettings.apply {
+            adapter = profileAdapter
+            layoutManager = LinearLayoutManager(context)
+        }.also {
+            initialiseSettingsItems()
+        }
+        settingsItemListLiveData.observe(viewLifecycleOwner, {
+            profileAdapter.differ.submitList(it)
+
+        })
+
+    }
+
+    private fun initialiseSettingsItems() {
+
+        if (user != null) {
+            with(user) {
+                lateinit var settingsName: ProfileSettingsItem
+
+                if (this.displayName?.isNotEmpty() == true) {
+                    settingsName = ProfileSettingsItem(1, "Name", displayName)
+                } else {
+                    with(StringTokenizer(email, "@")) {
+                        val username: String = this.nextToken()
+                        settingsName = ProfileSettingsItem(1, "Name", username)
+                    }
+                }
+
+                val settingsImage: ProfileSettingsItem = if (this.photoUrl != null) {
+                    ProfileSettingsItem(2, "Image", this.photoUrl?.toString()!!)
+                } else {
+                    ProfileSettingsItem(2, "Image", "N/A")
+                }
+
+                val settingsPhoneNumber: ProfileSettingsItem = if (this.phoneNumber != null && this.phoneNumber?.isNotEmpty() == true) {
+                    ProfileSettingsItem(3, "Phone Number", this.phoneNumber?.toString()!!)
+                } else {
+                    ProfileSettingsItem(3, "Phone Number", "N/A")
+                }
+
+                val settingsEmail: ProfileSettingsItem = if (this.phoneNumber != null && this.phoneNumber?.isNotEmpty() == true) {
+                    ProfileSettingsItem(4, "Email", email!!)
+                } else {
+                    ProfileSettingsItem(4, "Email", "N/A")
+                }
+                settingsItemListLiveData.value = listOf(settingsName, settingsImage, settingsPhoneNumber, settingsEmail)
+            }
+        }
     }
 
     private fun setData() {
-        if (auth.currentUser != null) {
-            with(auth.currentUser!!) {
+        if (user != null) {
+            with(user) {
                 if (this.displayName?.isNotEmpty() == true) {
                     tvUserDisplayName.text = this.displayName
                 } else {
