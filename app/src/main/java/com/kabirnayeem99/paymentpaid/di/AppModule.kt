ae3d2d271bc60198b4a2dc06e992949d79ebd6fa
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-04-08 22:21:25 +0600

    📦 feat (di): setup dependency injection for project
    add dagger hilt libraries in build.gradle
    create application class PpayedApplication.kt
    create module class AppModule.kt

---
+++ app/src/main/java/com/kabirnayeem99/paymentpaid/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.kabirnayeem99.paymentpaid.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
